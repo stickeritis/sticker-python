283f8affd097e4c8415011df556823a34e8e05e5
Daniël de Kok <me@danieldk.eu>
2019-07-19 08:23:28 +0200

    Add Sentence and Token types

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 use pyo3::prelude::*;
 
+mod util;
+pub use util::ListVec;
+
+mod sentence;
+pub use sentence::{PySentence, PyToken};
+
 /// This is a Python module for wrapping the sticker sequence labeler.
 #[pymodule]
-fn sticker(_py: Python, _m: &PyModule) -> PyResult<()> {
+fn sticker(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PySentence>()?;
+    m.add_class::<PyToken>()?;
+
     Ok(())
 }
