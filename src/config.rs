736fd45c52819bd416f1a49b7b4597f2ae0dabe7
Daniël de Kok <me@danieldk.eu>
2019-07-20 17:47:20 +0200

    Add Config type

---
+++ src/config.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::ops::Deref;
+
+use pyo3::exceptions;
+use pyo3::prelude::*;
+
+use sticker_utils::{Config, TomlRead};
+
+/// Config(file)
+/// --
+///
+/// Tagger configuration.
+#[pyclass(name=Config)]
+pub struct PyConfig {
+    inner: Config,
+}
+
+#[pymethods]
+impl PyConfig {
+    #[new]
+    fn __new__(obj: &PyRawObject, path: &str) -> PyResult<()> {
+        let reader = BufReader::new(File::open(path).map_err(|err| {
+            exceptions::IOError::py_err(format!(
+                "cannot read sticker configuration: {}",
+                err.to_string()
+            ))
+        })?);
+        let mut config = Config::from_toml_read(reader).map_err(|err| {
+            exceptions::ValueError::py_err(format!(
+                "cannot read parse configuration: {}",
+                err.to_string()
+            ))
+        })?;
+
+        config.relativize_paths(path).map_err(|err| {
+            exceptions::IOError::py_err(format!("cannot relativize paths: {}", err.to_string()))
+        })?;
+
+        obj.init(PyConfig { inner: config });
+
+        Ok(())
+    }
+}
+
+impl Deref for PyConfig {
+    type Target = Config;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
