4ddd50d301ab9dab959401016bf670965624b9f1
Daniël de Kok <me@danieldk.eu>
2019-07-21 12:01:43 +0200

    Switch to Nix for CI builds
    This makes it much easier to set up dependencies (e.g. libtensorflow)
    correctly.

---
+++ nix/rust-nightly.nix
@@ -0,0 +1,19 @@
+{ fetchFromGitHub, callPackage, makeRustPlatform }:
+
+let
+  mozillaOverlay = fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
+    sha256 = "18h0nvh55b5an4gmlgfbvwbyqj91bklf1zymis6lbdh75571qaz0";
+  };
+  mozilla = callPackage "${mozillaOverlay.out}/package-set.nix" {};
+  rustNightly = (mozilla.rustChannelOf { date = "2019-02-07"; channel = "nightly"; }).rust;
+  rustPlatform = makeRustPlatform {
+    cargo = rustNightly;
+    rustc = rustNightly;
+  };
+in makeRustPlatform {
+  cargo = rustNightly;
+  rustc = rustNightly;
+}
