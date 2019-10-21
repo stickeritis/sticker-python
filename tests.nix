cc7f8d149886973f8a1c7cf35e1c701fda709d24
Daniël de Kok <me@danieldk.eu>
2019-10-21 11:00:07 +0200

    Silence pytest caching warnings

--- tests.nix
+++ tests.nix
@@ -13,7 +13,8 @@ let
 in runCommand "test-tagger" {
   nativeBuildInputs = [ python3Packages.pytest sticker-python ];
 } ''
-  pytest --tagger_model=${models.de-pos-ud.model}/share/sticker/models/de-pos-ud/sticker.conf \
+  pytest -p no:cacheprovider \
+         --tagger_model=${models.de-pos-ud.model}/share/sticker/models/de-pos-ud/sticker.conf \
          --topo_model=${models.de-topo-ud-small.model}/share/sticker/models/de-topo-ud-small/sticker.conf \
     ${sticker-python.src}
   touch $out
